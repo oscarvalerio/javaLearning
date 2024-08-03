16e6d15c8dc28d2e0784aa893d2283f9f2e50fe0
oscarvalerio <oscarvalerio@gmail.com>
2024-08-02 21:16:08 -0600

    Adding a few more exercises

--- src/exercises.java
+++ src/exercises.java
@@ -1,7 +1,39 @@
 public class exercises {
     public static void main(String[] args) {
-        System.out.println(hasSharedDigit(19,23));
+        printFactors(-10);
     }
+
+    public static void printFactors(int number){
+        if(number<1){
+            System.out.println("Invalid Value");
+        }
+        for (int i=1;i<=number;i++){
+            if(number%i==0){
+                System.out.print(i+" ");
+            }
+        }
+    }
+
+    //Check if 2 numbers end with the same digit
+    public static boolean hasSameLastDigit(int num1, int num2, int num3){
+        if(num1<10||num1>1000||num2<10||num2>1000||num3<10||num3>1000){
+            return false;
+        }
+        int last1 = num1%10;
+        int last2 = num2%10;
+        int last3 = num3%10;
+        if(last1==last2) return true;
+        if(last1==last3) return true;
+        if(last3==last2) return true;
+        else return false;
+    }
+    public static boolean isValid(int number){
+        if(number<10||number>1000){
+            return false;
+        } else return true;
+    }
+
+
 
     //Check if the digit is present in both parameters
     public static boolean hasSharedDigit(int firstNum, int secondNum){
